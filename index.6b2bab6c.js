840e04b1eca80c1c14911a14133c558f510e32df
TatianaKh0301 <92024678+TatianaKh0301@users.noreply.github.com>
2023-08-03 11:23:24 +0000

    Deploying to gh-pages from @ TatianaKh0301/asl-s@fe0b02844f861b69bb83c0f4fe7afff7175ce2ae 🚀

---
+++ index.6b2bab6c.js
@@ -0,0 +1,2 @@
+new Swiper(".mySwiper",{autoHeight:!0,slideToClickedSlide:!0,keyboard:{enabled:!0,onlyInViewport:!0,pageUpDown:!0},mousewheel:{sensivity:1},autoplay:{delay:3e3,stopOnLastSlide:!1,disableOnInteraction:!1}});
+//# sourceMappingURL=index.6b2bab6c.js.map
--- index.b06a1c45.js
+++
@@ -1,2 +0,0 @@
-new Swiper(".mySwiper",{pagination:{el:".swiper-pagination",clickable:!0},autoHeight:!0,slideToClickedSlide:!0,keyboard:{enabled:!0,onlyInViewport:!0,pageUpDown:!0},mousewheel:{sensivity:1},autoplay:{delay:1e3,stopOnLastSlide:!1,disableOnInteraction:!1}});
-//# sourceMappingURL=index.b06a1c45.js.map
