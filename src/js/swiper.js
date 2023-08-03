c70047e7da1f540052cf3dd1ebbc168987d8f56d
TatianaKh0301 <92024678+TatianaKh0301@users.noreply.github.com>
2023-08-03 14:06:55 +0300

    update

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -1,7 +1,28 @@
-new Swiper(".image-slider", {
-    // pagination: {
-    //     el: ".swiper-pagination",
-    //     clickable: true,
-    //     dynamicBullet: true,
-    // },
+
+new Swiper(".mySwiper", {
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+
+    autoHeight: true,
+    
+    slideToClickedSlide: true,
+
+    keyboard: {
+        enabled: true,
+        onlyInViewport: true, 
+        pageUpDown: true,
+    },
+
+    mousewheel: {
+        sensivity: 1,
+    },
+
+    autoplay: {
+        delay: 1000,
+        stopOnLastSlide: false,
+        disableOnInteraction: false,
+    }
 });
+
