70bb6b82ce9b92592428d7f5d52fded700f3e47e
TatianaKh0301 <92024678+TatianaKh0301@users.noreply.github.com>
2023-08-09 17:53:39 +0300

    update

---
+++ src/js/callbackform.js
@@ -0,0 +1,36 @@
+(() => {
+    const refsModalCallback = {
+        openModalCallbackBtn: document.querySelector('.modal-open-btn'),
+        closeModalCallbackBtn: document.querySelector('.modal-close-btn'),
+        modalCallback: document.querySelector('.backdrop-send-form'),
+        body: document.querySelector('body'),
+    };
+    const { openModalCallbackBtn, closeModalCallbackBtn, modalCallback, body } =
+    refsModalCallback;
+    openModalCallbackBtn.addEventListener('click', onOpenModalCallbackBtn);
+    closeModalCallbackBtn.addEventListener('click', onCloseModalCallbackBtn);
+    modalCallback.addEventListener('click', onBackdropCallbackClick);
+
+    function onCloseModalCallbackBtn(e) {
+        modalCallback.classList.toggle('is-hidden');
+        body.classList.toggle('no-scroll');
+        window.removeEventListener('keydown', onEscKeyPress);
+    }
+
+    function onOpenModalCallbackBtn(e) {
+        window.addEventListener('keydown', onEscKeyPress);
+        modalCallback.classList.toggle('is-hidden');
+        body.classList.toggle('no-scroll');
+    }
+    function onEscKeyPress(e) {
+        if (e.key === 'Escape') {
+            onCloseModalCallbackBtn();
+        }
+    }
+
+    function onBackdropCallbackClick(e) {
+        if (e.currentTarget === e.target) {
+            onCloseModalCallbackBtn();
+        }
+    }
+})();
